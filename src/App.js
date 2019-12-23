142314f37d2e86738d36b465df38d014666a0e0f
Adson Wesley <adsonxzx@gmail.com>
2019-12-23 18:12:52 -0300

    implementando rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes';
 
 function App() {
-  return <h1>opa asas</h1>;
+  return (
+    <>
+      <h1>Pagina principal</h1>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
